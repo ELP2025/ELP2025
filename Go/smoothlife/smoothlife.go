0bb90ea8ec82445759c2ccda20262bbe99db29da
Leonigodas <eloi18.filipozzi2@gmail.com>
2025-01-16 10:21:30 +0100

    func sigmoid

--- Go/smoothlife/smoothlife.go
+++ Go/smoothlife/smoothlife.go
@@ -1,31 +1,34 @@
 package smoothlife
 
 import (
-  "math/rand"
+	"math"
+	"math/rand"
 )
 
-var(
-  width = 1000
-  height = 1000
+var (
+	width  = 1000
+	height = 1000
 )
 
 // genere des pixels avec une couleur random
 func GenerateRandomPixels(grid_width int, grid_height int) [][][]uint8 {
 	width = grid_width
-  height = grid_height
-  nestedPixels := make([][][]uint8, height)
+	height = grid_height
+	nestedPixels := make([][][]uint8, height)
 	for y := range nestedPixels {
 		nestedPixels[y] = make([][]uint8, width)
 		for x := range nestedPixels[y] {
-      aliveness := rand.Float32()/2
-				nestedPixels[y][x] = []uint8{
-					uint8(255*aliveness), // R
-					uint8(255*aliveness), // G
-					uint8(255*aliveness), // B
-        }
-    }
-  }
+			aliveness := rand.Float32() / 2
+			nestedPixels[y][x] = []uint8{
+				uint8(255 * aliveness), // R
+				uint8(255 * aliveness), // G
+				uint8(255 * aliveness), // B
+			}
+		}
+	}
 	return nestedPixels
 }
 
-
+func sigmoid(x, threshold, steepness) {
+	return 1 / (1 + math.Exp(-steepness*(x-threshold)))
+}
