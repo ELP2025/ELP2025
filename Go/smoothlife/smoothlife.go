345859babf628bf92d96021057c9407cba4bbeab
Valdonis2 <baptiste.sanchez@insa-lyon.fr>
2025-01-16 11:12:27 +0100

    Bug fixes

--- Go/smoothlife/smoothlife.go
+++ Go/smoothlife/smoothlife.go
@@ -3,65 +3,68 @@ package smoothlife
 import (
 	"math"
 	"math/rand"
-	"math"
-	"math/rand"
 )
 
 var (
 	width  = 1000
 	height = 1000
-var (
-	width  = 1000
-	height = 1000
 )
 
-func circleKernel(radius float32,center_x int, center_y int) []struct {Y; X int} {
-  result := make([]struct{Y; X int})
-  for x <= center_x - radius; x >= center_x + radius; x++ { //Reducing the interval for our search
-    for y <= center_y - radius; y >= center_y + radius; y++ { //Same
-      if sqrt((x-center_x)**2 + (y-center_y)**2) < radius { // If (x,y) is in circle of radius 
-        result = append(result, struct {Y; X int}{Y : y, X : x})
-      }
-    }
-  }
-  return result
+func circleKernel(radius float64, center_x int, center_y int) []struct {
+	Y int
+	X int
+} {
+	result := []struct {
+		Y int
+		X int
+	}{}
+	for x := float64(center_x) - radius; x >= float64(center_x)+radius; x++ { //Reducing the interval for our search
+		for y := float64(center_y) - radius; y >= float64(center_y)+radius; y++ { //Same
+			if math.Sqrt((x-float64(center_x))*(x-float64(center_x))+(y-float64(center_y))*(y-float64(center_y))) < radius { // If (x,y) is in circle of radius
+				result = append(result, struct {
+					Y int
+					X int
+				}{Y: int(y), X: int(x)})
+			}
+		}
+	}
+	return result
 }
 
-func wheightedConvolve(pixels [][][][]uint8, radius float32, x int, int y) float32 {
-  points := circleKernel(radius, x, y)
-  sum := 0.0
-  count :=0 
-  for _,point := range points {
-    px, py = point.X, point.Y
-    sum += getNeighborValue(pixels, x, y)
-    count +=1
-  }
+func wheightedConvolve(pixels [][][]uint8, radius float64, x int, y int) float64 {
+	points := circleKernel(radius, x, y)
+	sum := 0.0
+	count := 0
+	for _, point := range points {
+		px, py := point.X, point.Y
+		sum += float64(getNeighborValue(pixels, px, py))
+		count += 1
+	}
 
-  if count : return sum/count
-  return 0
-
+	if count > 0 {
+		return (sum / float64(count))
+	}
+	return 0
 }
 
-func getNeighborValue(pixels [][][]uint8, x int, y int) int {
-  //Returns the value of the selected pixel, if it overflows, it goes back to the start of the grid
-    if x > width {
-      x -= width
-    } else if x < 0 {
-      x += width 
-    }
-    if y > height {
-      y -= height
-    } else if y < 0 {
-      y += height
-    }
-    return pixels[y][x][0]
+func getNeighborValue(pixels [][][]uint8, x int, y int) uint8 {
+	//Returns the value of the selected pixel, if it overflows, it goes back to the start of the grid
+	if x > width {
+		x -= width
+	} else if x < 0 {
+		x += width
+	}
+	if y > height {
+		y -= height
+	} else if y < 0 {
+		y += height
+	}
+	return pixels[y][x][0]
 }
 
 // genere des pixels avec une couleur random
 func GenerateRandomPixels(grid_width int, grid_height int) [][][]uint8 {
 	width = grid_width
-	height = grid_height
-	nestedPixels := make([][][]uint8, height)
 	height = grid_height
 	nestedPixels := make([][][]uint8, height)
 	for y := range nestedPixels {
@@ -75,20 +78,15 @@ func GenerateRandomPixels(grid_width int, grid_height int) [][][]uint8 {
 			}
 		}
 	}
-			aliveness := rand.Float32() / 2
-			nestedPixels[y][x] = []uint8{
-				uint8(255 * aliveness), // R
-				uint8(255 * aliveness), // G
-				uint8(255 * aliveness), // B
-			}
-		}
-	}
+
 	return nestedPixels
 }
 
-func sigmoid(x float32, threshold float32, steepness float32) float32 {
+func sigmoid(x float64, threshold float64, steepness float64) float64 {
 	return 1 / (1 + math.Exp(-steepness*(x-threshold)))
-}func compute_new_state(S_n float32, S_m float32, B float32, S float32, K float32) uint8 {
+}
+
+func compute_new_state(S_n float64, S_m float64, B float64, S float64, K float64) uint8 {
 	birth := sigmoid(S_m, B, K)
 	survival := sigmoid(S_m, B, K)
 
